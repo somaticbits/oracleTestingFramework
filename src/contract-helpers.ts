761558b21da3b307f05b00ff288c2af3ab46141a
d-pettersson <david.pettersson@code.berlin>
2022-05-18 13:54:35 +0200

    Add async/await

--- src/contract-helpers.ts
+++ src/contract-helpers.ts
@@ -23,8 +23,8 @@ const readTzFile = (contractType: string) => {
   return { contract, storage };
 };
 
-const originateContract = async (signer: TezosToolkit, contract: string | object[], storage: string | object[]) => {
-  let address = null;
+const originateContract = async (signer: TezosToolkit, contract: string, storage: string): Promise<string> => {
+  let address: string | undefined = '';
   await signer.contract
     .originate({
       code: contract,
@@ -34,14 +34,20 @@ const originateContract = async (signer: TezosToolkit, contract: string | object
     .then((op) => {
       console.log(`Waiting for confirmation of origination for ${op.contractAddress}`);
       address = op.contractAddress;
+      op.confirmation(1).then(() => {
+        console.log(`Origination of ${address} confirmed`);
+      });
     })
-    .then(() => console.log(`Contract originated`))
-    .then(() => delay(5000))
+    .then(() => delay(6000))
     .catch((error) => console.error(`${JSON.stringify(error)}`));
   return address;
 };
 
-export const originateContracts = async () => {
+export const originateContracts = async (): Promise<{
+  bigmapContract: string;
+  mapContract: string;
+  recordBigmapContract: string;
+}> => {
   const Tezos = new TezosToolkit('http://localhost:20000');
   Tezos.setProvider({ signer: new InMemorySigner(skFlextesaAlice) });
 
@@ -51,23 +57,25 @@ export const originateContracts = async () => {
   const { contract: oMap, storage: oMapStorage } = readTzFile('oracle_map');
   const mapContract = await originateContract(Tezos, oMap, oMapStorage);
 
-  const { contract: oRecordBigmap, storage: oRecordBigmapStorage } = readTzFile('oracle_map');
+  const { contract: oRecordBigmap, storage: oRecordBigmapStorage } = readTzFile('oracle_record_bigmap');
   const recordBigmapContract = await originateContract(Tezos, oRecordBigmap, oRecordBigmapStorage);
 
   return { bigmapContract, mapContract, recordBigmapContract };
 };
 
-export const getCosts = (signer: TezosToolkit, contractAddress: string, data: number) => {
+export const getCosts = async (signer: TezosToolkit, contractAddress: string, data: number) => {
   let consumedGas = 0;
   let storageFee = 0;
+  let total = 0;
 
-  signer.contract
+  await signer.contract
     .at(`${contractAddress}`)
     .then((contract) => contract.methods.add_data(data, 0).send())
     .then((op) => {
       consumedGas = Number.parseFloat(op.consumedGas);
       storageFee = op.fee;
+      total = (op.fee + Number.parseFloat(op.consumedGas)) / 1e6;
     })
     .catch((error) => console.log(`Error: ${JSON.stringify(error, null, 2)}`));
-  return { consumedGas, storageFee };
+  return { consumedGas, storageFee, total };
 };
